17aa783159f82b17fd0399f8d99082555523e5eb
Mohit <mohit93934@gmail.com>
2022-04-09 22:02:20 +0530

    sudomain

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const userRoutes = require("./routes/userRoutes");
+
+app.get('/', (req, res)=> {
+    console.log(req.url)
+    console.log(req.subdomains)
+    return res.status(200).send({ success: true, message: "Welcome to App"});
+})
+app.use('/user', userRoutes);
+
 app.use('*', function (req, res) {
     res.status(404).send({ success: false, message: "Page Not Found", error: { name: "Error", message: "Invalid route" } });
 });
